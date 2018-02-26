a8545ebdd413f929a8e1d07cbe818c5786bbe417
Ralf Kruse <rkr@remmert.de>
2018-02-26 23:03:32 +0100

    Split

---
+++ project/typings/jsx/html-attributes.d.ts
@@ -0,0 +1,6 @@
+// Merged namespace: https://www.typescriptlang.org/docs/handbook/declaration-merging.html
+declare namespace JSX {
+    interface HTMLAttributes {
+        "custom_attribute"?: any;
+    }
+}
--- project/typings/jsx/intrinsic-elements.d.ts
+++ project/typings/jsx/intrinsic-elements.d.ts
@@ -1,4 +1,5 @@
 declare namespace JSX {
     interface IntrinsicElements {
+        "custom-element": any;
     }
 }
