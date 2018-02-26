b88d9b817027d603d70d68ec7cfe4d3430aa9e9e
Ralf Kruse <rkr@remmert.de>
2018-02-26 20:14:53 +0100

    Included dev-server section

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,22 @@
 const merge = require("webpack-merge");
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
+const os = require("os");
+
+var hostname = os.hostname();
+var port = 8080;
 
 module.exports = merge(common, {
     devtool: "source-map",
+    devServer: {
+        contentBase: "./dist",
+        compress: true,
+        inline: true,
+        host: "0.0.0.0",                // Make app available from external browsers...
+        port: port,                     // ...on the assigned port...
+        public: `${hostname}:${port}`,  // ...with the specified URL...
+        disableHostCheck: true          // ...and do not check hosts
+    },
     plugins: [
         new UglifyJSPlugin({
             sourceMap: true
