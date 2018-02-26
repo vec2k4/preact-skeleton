6c0bdbc8aea0a212d12cc0fb38cb94eb9f9dbbe1
Ralf Kruse <rkr@remmert.de>
2018-02-26 21:18:09 +0100

    Added PostCSS config for Autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: {
+        autoprefixer: {
+            browsers: [
+                // Select browsers: https://github.com/ai/browserslist#queries
+                "last 2 versions"
+            ]
+        }
+    }
+}
